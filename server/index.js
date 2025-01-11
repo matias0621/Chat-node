14047d529e8f073e5d3c7e0d653e5c654823b6c7
matias0621 <mattiascorrea@outlook.com>
2025-01-10 21:43:02 -0300

    messages ✅

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
+
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3000
 
@@ -11,16 +16,58 @@ const io = new Server(server, {
     connectionStateRecovery: {}
 })
 
-io.on('connection', (socket) => {
+const db = createClient({
+    url: "libsql://prime-night-nurse-matias0621.aws-us-east-1.turso.io",
+    authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT,
+        user TEXT
+    )
+`)
+
+io.on('connection', async (socket) => {
     console.log('a user has connected')
 
     socket.on('disconnect', () => {
         console.log('an user has disconnected')
     })
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg)
+    socket.on('chat message', async (msg) => {
+        let result
+        const username = socket.handshake.auth.username ?? 'antonio'
+        try {
+            result = await db.execute({
+                sql: 'INSERT INTO messages (content, user) VALUES (:msg, :username)',
+                args: { msg, username }
+            })
+        }catch (e){
+            console.error(e)
+            return
+        }
+        io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
     })
+
+    console.log("auth 👇")
+    console.log(socket.handshake.auth)
+
+    if (!socket.recovered){
+        try {
+            const result = await db.execute({
+                sql: 'SELECT * FROM messages WHERE id > ?',
+                args: [socket.handshake.auth.serverOffset ?? 0]
+            })
+
+            result.rows.forEach(row => {
+                socket.emit('chat message', row.content, row.id.toString(), row.user)
+            })
+        }catch (e){
+            console.error(e)
+        }
+    }
 })
 
 app.use(logger("dev"))
